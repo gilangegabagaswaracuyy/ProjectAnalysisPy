44af68796dc7647082d4d85976f89d3e9b31f63e
gilangegabagaswaracuyy <145256756+gilangegabagaswaracuyy@users.noreply.github.com>
2025-03-02 16:43:22 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -12,8 +12,8 @@ st.title("Dashboard Analisis Data E-Commerce")
 # Memuat dataset dengan caching
 @st.cache_resource
 def load_data():
-    day_file = pd.read_csv("data/day.csv")
-    hour_file = pd.read_csv("data/hour.csv")
+    day_file = pd.read_csv("../data/day.csv")
+    hour_file = pd.read_csv("../data/hour.csv")
     
     # Cek apakah file ada
     if not os.path.exists(day_file):
